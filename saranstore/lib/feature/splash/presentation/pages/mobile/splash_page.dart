f6abfdcd388d8888e34c3996574fecceb1ba6c07
Saran M K <mksaran0@gmail.com>
2026-06-02 22:48:25 +0530

    changed splash page with new animation

--- saranstore/lib/feature/splash/presentation/pages/mobile/splash_page.dart
+++ saranstore/lib/feature/splash/presentation/pages/mobile/splash_page.dart
@@ -1,37 +1,4 @@
-// import 'package:animated_splash_screen/animated_splash_screen.dart';
-// import 'package:flutter/material.dart';
-// import 'package:page_transition/page_transition.dart';
-// import 'package:saranstore/core/constant/app_palette.dart';
-// import 'package:saranstore/feature/home/presentation/pages/home_page.dart';
-
-// class SplashPage extends StatelessWidget {
-//   const SplashPage({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return AnimatedSplashScreen(
-//       duration: 4000,
-//       splash: Column(
-//         mainAxisAlignment: MainAxisAlignment.center,
-//         children: [
-//           ClipOval(
-//             child: Image.asset('assets/pngs/app_logo_2.png', width: 240),
-//           ),
-//         ],
-//       ),
-
-//       nextScreen: const HomePage(),
-
-//       splashIconSize: 310,
-
-//       backgroundColor: AppPalette.primaryColor,
-
-//       pageTransitionType: PageTransitionType.bottomToTop,
-
-//       animationDuration: const Duration(milliseconds: 700),
-//     );
-//   }
-// }
+import 'dart:math';
 
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
@@ -46,30 +13,29 @@ class SplashPage extends StatefulWidget {
   State<SplashPage> createState() => _SplashPageState();
 }
 
-class _SplashPageState extends State<SplashPage>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
+class _SplashPageState extends State<SplashPage> with TickerProviderStateMixin {
+  late AnimationController _clockwiseController;
+  late AnimationController _antiClockwiseController;
 
   @override
   void initState() {
     super.initState();
 
-    _controller = AnimationController(
+    _clockwiseController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    )..repeat();
+
+    _antiClockwiseController = AnimationController(
       vsync: this,
       duration: const Duration(seconds: 5),
-    );
-
-    // Start effect after 700ms
-    Future.delayed(const Duration(milliseconds: 700), () {
-      if (mounted) {
-        _controller.repeat();
-      }
-    });
+    )..repeat();
   }
 
   @override
   void dispose() {
-    _controller.dispose();
+    _clockwiseController.dispose();
+    _antiClockwiseController.dispose();
     super.dispose();
   }
 
@@ -77,24 +43,37 @@ class _SplashPageState extends State<SplashPage>
   Widget build(BuildContext context) {
     return AnimatedSplashScreen(
       duration: 4000,
+      backgroundColor: AppPalette.primaryColor,
+      splashIconSize: 380,
 
       splash: AnimatedBuilder(
-        animation: _controller,
+        animation: Listenable.merge([
+          _clockwiseController,
+          _antiClockwiseController,
+        ]),
         builder: (context, child) {
           return Stack(
             alignment: Alignment.center,
             children: [
-              // Animated glowing circle effect
-              Container(
-                width: 260 + (_controller.value * 40),
-                height: 260 + (_controller.value * 40),
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: AppPalette.secondaryColor,
+              // Outer Ring (Clockwise)
+              Transform.rotate(
+                angle: _clockwiseController.value * 2 * pi,
+                child: CustomPaint(
+                  size: const Size(340, 340),
+                  painter: DottedCirclePainter(dotCount: 48, dotRadius: 4),
                 ),
               ),
 
-              // Main logo
+              // Inner Ring (Anti-Clockwise)
+              Transform.rotate(
+                angle: -_antiClockwiseController.value * 2 * pi,
+                child: CustomPaint(
+                  size: const Size(280, 280),
+                  painter: DottedCirclePainter(dotCount: 36, dotRadius: 3.5),
+                ),
+              ),
+
+              // Logo
               ClipOval(
                 child: Image.asset('assets/pngs/app_logo_2.png', width: 240),
               ),
@@ -104,14 +83,40 @@ class _SplashPageState extends State<SplashPage>
       ),
 
       nextScreen: const HomePage(),
-
-      splashIconSize: 320,
-
-      backgroundColor: AppPalette.primaryColor,
-
       pageTransitionType: PageTransitionType.bottomToTop,
-
-      animationDuration: const Duration(milliseconds: 700),
+      animationDuration: const Duration(milliseconds: 1000),
     );
   }
+}
+
+class DottedCirclePainter extends CustomPainter {
+  final int dotCount;
+  final double dotRadius;
+
+  DottedCirclePainter({required this.dotCount, required this.dotRadius});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = AppPalette.secondaryColor
+      ..style = PaintingStyle.fill;
+
+    final center = Offset(size.width / 2, size.height / 2);
+    final radius = size.width / 2 - 10;
+
+    for (int i = 0; i < dotCount; i++) {
+      final angle = (2 * pi * i) / dotCount;
+
+      final x = center.dx + radius * cos(angle);
+      final y = center.dy + radius * sin(angle);
+
+      // Alternate dot sizes for a premium look
+      final currentRadius = i % 4 == 0 ? dotRadius * 1.6 : dotRadius;
+
+      canvas.drawCircle(Offset(x, y), currentRadius, paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
 }
